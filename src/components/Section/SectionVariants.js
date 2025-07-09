e261875594978a08fc48185465a84f4b45e3b425
griffinjoshs <leveloutusa@gmail.com>
2025-07-09 13:18:42 -0400

    hero

--- src/components/Section/SectionVariants.js
+++ src/components/Section/SectionVariants.js
@@ -10,15 +10,13 @@ export const SectionVariants = {
    * Primary Hero variant – full-screen hero with background image, overlay, heading, description, and buttons.
    */
   primaryHero: {
-    sectionClass:
-      "flex flex-col items-center min-h-screen justify-end [background-image:var(--heroGradient)] bg-cover bg-fixed bg-center z-10 text-xl",
-    contentClass: "relative md:py-0 w-auto h-full flex flex-col md:flex-row z-20 md:gap-[var(--spacing-xs)]",
-    topContentClass:
-      "basis-4/7 w-80/100 mx-auto md:w-auto flex items-start justify-center md:justify-center flex-col pt-[100px] md:pl-[80px]",
-    imageColumnClass: "basis-3/7 flex items-end justify-center md:justify-end",
-    buttonsPlacement: "top-content-section",
-    headingAreaClass: "space-y-[var(--spacing-md)]",
-    descriptionClass: "md:pr-[var(--spacing-xl)] font-thin text-lg md:text-xl",
+    sectionClass:      "flex flex-col items-center min-h-screen justify-end [background-image:var(--heroGradient)] bg-cover bg-fixed bg-center z-10 text-xl",
+  contentClass:      "relative md:py-0 w-auto h-full flex flex-col md:flex-row z-20 md:gap-[var(--spacing-xs)]",
+  topContentClass:   "basis-4/7 w-80/100 mx-auto md:w-auto flex items-start justify-center md:justify-center flex-col pt-[100px] md:pl-[80px]",
+  imageColumnClass:  "flex items-end justify-center mx-auto max-w-xs md:basis-3/7 md:max-w-none md:justify-end",
+  buttonsPlacement:  "top-content-section",
+  headingAreaClass:  "space-y-[var(--spacing-md)]",
+  descriptionClass:  "md:pr-[var(--spacing-xl)] font-thin text-lg md:text-xl",
   },
 
   /**
